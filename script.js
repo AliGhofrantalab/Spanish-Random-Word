3099c662ecd1fc02dbf29cc04ec95cb325b7dd46
AliGhofrantalab <a.ghofrantalab@gmail.com>
2026-06-01 09:10:47 +0330

    v1.1 improve UX and prevent duplicate words
    Add lastIndex to avoid repeating words and auto-load first word

--- script.js
+++ script.js
@@ -25,19 +25,23 @@ const wordE= document.getElementById("word")
 const meaningE = document.getElementById("meaning")
 const exampleE = document.getElementById("example")
 const btn = document.getElementById("btn")
+let lastIndex = -1
 btn.addEventListener("click" ,showWord )
-function showWord(){
-const randomIndex = 
-Math.floor(Math.random() * words.length)
-    const selectedWord = words[randomIndex]
-    wordE.textContent =
-    selectedWord.word
+function showWord() {
 
-    meaningE.textContent =
-    selectedWord.meaning
+  let randomIndex
 
-    exampleE.textContent =
-    selectedWord.example
-    }
+  do {
+    randomIndex = Math.floor(Math.random() * words.length)
+  } while (randomIndex === lastIndex)
+
+  lastIndex = randomIndex
+
+  const selectedWord = words[randomIndex]
+
+  wordE.textContent = selectedWord.word
+  meaningE.textContent = selectedWord.meaning
+  exampleE.textContent = selectedWord.example
+}
     showWord()
     window.onload=showWord;
